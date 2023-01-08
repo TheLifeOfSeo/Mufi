6b40c19e5112160f30396d014f3c23ff4b50fc26
Seo <dsa3182@gmail.com>
2023-01-08 21:04:20 +0900

    Update : New Webapp Layout on Svelte

---
+++ views/content/Store.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Card from "../layout/component/Card.svelte";
+
+</script>
+
+<Card>
+    <div>
+        <h2>매장 통합 관리</h2>
+    </div>
+    <div>
+        <button id="btn-add-store" class="uk-button uk-button-primary" href="#store-modal" uk-toggle>매장 추가하기</button>
+    </div>
+    <div>
+        <table class="uk-table uk-table-hover uk-table-small uk-table-middle uk-table-divider">
+            <thead>
+                <tr>
+                    <th>상점 이름</th>
+                    <th>메뉴 추가</th>
+                    <th>메뉴 보기</th>
+                    <th>설정</th>
+                </tr>
+            </thead>
+            <tbody id="table-data"></tbody>
+        </table>
+    </div>
+</Card>
+<Card>
+        <div class="uk-flex uk-flex-between">
+            <div>
+                <h2>판매중인 상품 조회</h2>
+            </div>
+            <div id="btn-div-menu-close">
+                <button class="uk-close-large" type="button" uk-close></button>
+            </div>
+        </div>
+        <!-- <div>
+            <button id="btn-add-menu" class="uk-button uk-button-primary" href="#menu-modal" uk-toggle>상품 추가</button>
+        </div> -->
+        <!-- <select class="uk-select" id="store-list">
+            <option>매장 선택</option>
+        </select> -->
+        <div id="menu-data">
+        </div>
+</Card>
---
+++ views/layout/component/Card.svelte
@@ -0,0 +1,24 @@
+<div id="card-body">
+    <div>
+        <slot></slot>
+    </div>
+</div>
+
+<style>
+    @media (min-width: 769px) {
+        #card-body {
+            margin: 18px 18px;
+            border-radius: 24px;
+            box-shadow: 0px 4px 18px 0px #DDDDDD;
+        }
+    }
+
+    @media (max-width: 768px) {
+    
+    }
+    #card-body {
+        padding: 36px;
+        /* background-color:aliceblue; */
+        /* border-radius: 24px; */
+    }
+</style>
